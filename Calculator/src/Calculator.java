41d271d7ede1b96c80190718a4ac0b60f9a26e24
Siarhei Malakhouski <Malahovskys@users.noreply.github.com>
2015-03-24 13:11:16 +0300

    Update Calculator.java

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -3,19 +3,25 @@ public class Calculator {
 	
 	public Calculator() {}
 	
+	//TODO Will be better use double for input parameters
+	//TODO Incorrect variable naming
 	public void operation(int x2, String sign2, int y2) {
 		double res;
 		switch(sign2) {
+		//TODO There is required tab.
 		case "+": 
 			res = x2+y2;
+			//TODO Can be placed after switch or moved to another print method
 			System.out.println(x2 + " " + sign2 + " " + y2 + " = " + (int)res);
 			break;
 		case "-":
 			res = x2-y2;
+			//TODO Can be placed after switch or moved to another print method
 			System.out.println(x2 + " " + sign2 + " " + y2 + " = " + (int)res);
 			break;
 		case "*": 
 			res = x2*y2;
+			//TODO Can be placed after switch or moved to another print method
 			System.out.println(x2 + " " + sign2 + " " + y2 + " = " + (int)res);
 			break;
 		case "/": 
@@ -24,10 +30,12 @@ public class Calculator {
 			} 
 			else {
 				res = (double)x2/(double)y2;
+				//TODO Can be placed after switch or moved to another print method
 				System.out.println(x2 + " " + sign2 + " " + y2 + " = " + res);
 			}
 			break;
 		default:
+			//TODO Throw exception about not supported method
 			break;
 		}
 
